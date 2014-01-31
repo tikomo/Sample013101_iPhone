5deec54117af814b782a0ffce7052cb6696680c7
tikomo <tikomo@Komorita-no-MacBook-Air.local>
2014-01-31 14:18:19 +0900

    Initial Commit

---
+++ Sample013101/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sample013101
+//
+//  Created by tikomo on 2014/01/31.
+//  Copyright (c) 2014年 tikomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Sample013101Tests/Sample013101Tests.m
@@ -0,0 +1,34 @@
+//
+//  Sample013101Tests.m
+//  Sample013101Tests
+//
+//  Created by tikomo on 2014/01/31.
+//  Copyright (c) 2014年 tikomo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Sample013101Tests : XCTestCase
+
+@end
+
+@implementation Sample013101Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
